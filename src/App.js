8a3413d4396d7140b175352d382503dd25956ee1
mevnik <mevnik@gmail.com>
2023-08-29 08:55:13 +0200

    add webApp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, {useEffect} from 'react';
+
 import './App.css';
+const tg = window.Telegram.WebApp
 
 function App() {
+  const onClose = () => {tg.close()}
+  useEffect(() => {
+    tg.ready()
+  },[])
+
   return (
     <div className="App">
       HELLO!
+      <button onClick={onClose}>Close</button>>
     </div>
   );
 }
