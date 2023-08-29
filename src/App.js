584db50f2772fbbb9f87fc7d02de91d8eab12f58
mevnik <mevnik@gmail.com>
2023-08-29 13:06:16 +0200

    add onClose

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import Header from './Components/Header/header.jsx';
-
+// https://www.w3schools.com/jsref/jsref_operators.asp
 import './App.css';
 const tg = window.Telegram.WebApp
 
@@ -10,9 +10,12 @@ function App() {
     tg.ready()
   },[])
 
+  const name = tg.initDataUnsafe?.user?.username;
+  const username = name? name: 'Jon';
+
   return (
     <div className="App">
-      <Header onClose = {onClose} username = {tg.initDataUnsafe?.user?.username}/>
+      <Header onClose = {onClose} username = {username}/>
 
     </div>
   );
--- src/Components/Button/button.jsx
+++ src/Components/Button/button.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './button.css'
 
 const Button = (props) => {
-	return <button {...props} className={`button`}>{props.text}</button>;
+	return <button {...props} onClick = {props.onClose} className={`button`}>{props.text}</button>;
 }
 
 export default Button;
