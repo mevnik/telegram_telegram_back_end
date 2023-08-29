90ffc05d06560675bae9cd90f09f0125bd00c49a
mevnik <mevnik@gmail.com>
2023-08-29 14:20:52 +0200

    add toggleButton

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React, {useEffect} from 'react';
 import Header from './Components/Header/header.jsx';
 // https://www.w3schools.com/jsref/jsref_operators.asp
 import './App.css';
-const tg = window.Telegram.WebApp
+import {useTelegram} from './Hooks/useTelegram.jsx'
 
 function App() {
-  const onClose = () => {tg.close()}
+const {name,onClose,tg,onToggleButton} = useTelegram()
   useEffect(() => {
     tg.ready()
-  },[])
+  },[tg])
 
-  const name = tg.initDataUnsafe?.user?.username;
   const username = name? name: 'Jon';
 
   return (
     <div className="App">
       <Header onClose = {onClose} username = {username}/>
-
+      <button onClick = {onToggleButton}>toggle</button>
     </div>
   );
 }
---
+++ src/Hooks/useTelegram.jsx
@@ -0,0 +1,21 @@
+const tg = window.Telegram.WebApp
+
+
+
+const name = tg.initDataUnsafe?.user?.username;
+const onClose = () => {tg.close()};
+const onToggleButton = () => {
+	if(tg.MainButton.isVisible){
+		tg.MainButton.hide()
+	}
+	else tg.MainButton.show()
+
+}
+export function useTelegram(){
+	return({
+		tg,
+		onToggleButton,
+		onClose,
+		name,
+	})
+}
