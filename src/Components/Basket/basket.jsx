81eb18a2059b7b5cbd83646ad89f4297ea68f0c3
mevnik <mevnik@gmail.com>
2023-09-04 11:36:20 +0200

    add go to basket15

--- src/Components/Basket/basket.jsx
+++ src/Components/Basket/basket.jsx
@@ -8,7 +8,7 @@ export const Basket = (props) => {
 	return (
 		<div className = {'container'}>
 			<p>It's basket</p>
-			<p>props.count</p>
+			<p>{props.count}</p>
 
 
 			{ props.basket.map((item) => 
