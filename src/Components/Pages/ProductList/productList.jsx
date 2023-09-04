709ed78674b1d137f06fda6efd0c8a687a206991
mevnik <mevnik@gmail.com>
2023-09-04 09:35:29 +0200

    add go to basket9

--- src/Components/Pages/ProductList/productList.jsx
+++ src/Components/Pages/ProductList/productList.jsx
@@ -46,31 +46,55 @@ export const ProductList = () => {
 	    */
 
 	const[all_price, setAll_price] = useState(0)
+	const[basket, setBasket] = useState(false)
 
 	const editPrice = (value) => {
 			if(all_price === 0) onToggleButton()
 			setAll_price(all_price + value)
 	}
 
-	return(
+	const goBasket = () => {
+		setBasket(true)
 
-			<div>
-				<div className="listContainer">
-					{
-						Products.map((item) => 
-							<Item key = {item.id} 
-							title = {item.title} 
-							description = {item.description}
-							 price = {item.price} 
-							 img = {item.img}
-							 editPrice = {editPrice}
-							 />
-						)
-					}
+	}
+
+	useEffect(() => {
+    	tg.onEvent('mainButtonClicked', goBasket)
+    	return () => {
+    		tg.offEvent('mainButtonClicked', goBasket)
+    	}
+        
+    },[tg])
+
+	if(!basket){
+		return(
+
+				<div>
+
+
+					<div className="listContainer">
+						{
+							Products.map((item) => 
+								<Item key = {item.id} 
+								title = {item.title} 
+								description = {item.description}
+								 price = {item.price} 
+								 img = {item.img}
+								 editPrice = {editPrice}
+								 />
+							)
+						}
+					</div>
+					<div className = {'Item'}>Sum in basket:{all_price}$</div>
 				</div>
-				<div className = {'Item'}>Sum in basket:{all_price}$</div>
 
-			</div>
 
-		)
+			)
+		}
+				
+
+		return (
+			<div>It's basket</div>
+			)
+
 	}
