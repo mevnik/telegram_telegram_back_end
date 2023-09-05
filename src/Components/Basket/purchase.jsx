c3094965ed70de2ce2e013091963113349018d6e
mevnik <mevnik@gmail.com>
2023-09-05 09:42:34 +0200

    add go to basket31

--- src/Components/Basket/purchase.jsx
+++ src/Components/Basket/purchase.jsx
@@ -9,13 +9,28 @@ export const Purchase = (props) => {
 	return (
 		<div className = {'container'}>
 			<div className = {'purchase'}>
+				<div className = >eee</div>
+				<div className = {'result'}>
+					<div className = {'block_mini'}>Price:{product.product.price}</div>
+					<div className = {'block_mini'}>Sum:123</div>
+				</div>
+			</div>
+		</div>
+
+		)
+
+/*
+	return (
+		<div className = {'container'}>
+			<div className = {'purchase'}>
 				<div className = {'img'}><img src = {product.img} alt = 'purchase'/></div>
 				<div className = {'result'}>
-					<div className = {'block_mini'}>Pric:{product.price}</div>
+					<div className = {'block_mini'}>Price:{product.price}</div>
 					<div className = {'block_mini'}>Sum:{ product.price * props.product.click}</div>
 				</div>
 			</div>
 		</div>
 
 		)
+		*/
 }
